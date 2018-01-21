48526e8250afda945760de526c3734513ec7d75d
lykss <252205451@qq.com>
2018-01-21 17:57:21 +0800

    代码分割和懒加载

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 > Webpack demo project.
 
+## 参考资料
+
+<https://www.cnblogs.com/skylor/p/7008756.html>
+
 ## Typings
 
 ``` bash
@@ -9,3 +13,22 @@ npm install typings
 
 typings install lodash
 ```
+
+## webpack代码分割以及懒加载
+
+### 实现方式
+
+webpack methods
+  require.ensure
+  require.include
+
+ES2015 Loader Spec
+  System.import() -> import()
+  import() -> Promise
+  import().then()
+
+### 应用场景
+
+  分离业务代码和第三方依赖
+  分离业务代码、业务公共代码和第三方依赖
+  分离首次加载和访问后加载的代码
--- src/page-a.js
+++ src/page-a.js
@@ -1,6 +1,20 @@
-import './subpage-a'
-import './subpage-b'
+require.include('./module-a')
 
-import * as _ from 'lodash'
+var page = 'subpageA'
+
+if (page === 'subpageA') {
+  import(/* webpackChunkName:'subpageA' */'./subpage-a').then(function(subPageA) {
+    console.log(subpageA)
+  })
+} else if (page === 'subpageB') {
+  import(/* webpackChunkName:'subpageA' */'./subpage-b').then(function(subPageB) {
+    console.log(subpageB)
+  })
+}
+
+require.ensure([], function() {
+  var _ = require('lodash')
+  _.join([1, 2, 3], ',')
+}, 'vendor')
 
 export default 'pageA'
